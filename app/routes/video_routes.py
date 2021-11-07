daabfd874684010dd01c0a658e93796575c3b3fa
Jessica <jessica.w.roberts@gmail.com>
2021-11-07 09:48:50 -0800

    Add create_video route

--- app/routes/video_routes.py
+++ app/routes/video_routes.py
@@ -2,6 +2,7 @@ from flask import Blueprint, jsonify, make_response, request, abort
 from flask.helpers import make_response
 from flask.json import tojson_filter
 from flask.signals import request_tearing_down
+from werkzeug.exceptions import RequestedRangeNotSatisfiable
 from werkzeug.utils import header_property
 from app.models.video import Video
 from app import db
@@ -19,4 +20,22 @@ def valid_int(number, parameter_type):
 def get_vidoe_from_id(video_id):
     valid_int(video_id, "video_id")
     return Video.query.get_or_404(video_id, description="{video not found}")
+
+
+# Routes
+@video_bp.route("", methods=["POST"])
+def create_video():
+    request_body = request.get_json()
+    if "title" not in request_body or "release_date" not in request_body or "total_inventory" not in request_body:
+        return make_response({"detail": "Invalid data"}, 400)
     
+    new_video = Video(
+        title=request_body["title"],
+        release_date=request_body["release_date"],
+        total_inventory=request_body["total_inventory"]
+    )
+
+    db.session.add(new_video)
+    db.session.commit()
+
+    return make_response({"video": new_video.to_dict()}, 201)
