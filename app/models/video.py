3cb076fcf5e24adc38442f96246ea3f16aab723e
Jessica <jessica.w.roberts@gmail.com>
2021-11-07 09:23:20 -0800

    Add to_dict method for Video class

--- app/models/video.py
+++ app/models/video.py
@@ -5,4 +5,11 @@ class Video(db.Model):
     title = db.Column(db.String, nullable=False)
     release_date = db.Column(db.DateTime, nullable=False)
     total_inventory = db.Column(db.Integer, nullable=True)
-    
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "release_date": self.release_date,
+            "totoal_inventory": self.total_inventory
+        }
