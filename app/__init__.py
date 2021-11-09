3a9190b8db0334fc3fed857ac7ff5cecf6f4b6f5
Makhabat <maksatbekova.m@gmail.com>
2021-11-08 22:36:42 -0800

    CRUD for customer model was created

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ import os
 from dotenv import load_dotenv
 
 db = SQLAlchemy()
-migrate = Migrate()
+migrate = Migrate(compare_type=True)
 load_dotenv()
 
 def create_app(test_config=None):
@@ -32,5 +32,7 @@ def create_app(test_config=None):
     migrate.init_app(app, db)
 
     #Register Blueprints Here
+    from .routes.customer_routes import customer_bp
+    app.register_blueprint(customer_bp)
 
     return app
--- app/models/customer.py
+++ app/models/customer.py
@@ -1,4 +1,19 @@
+from flask import current_app
 from app import db
 
 class Customer(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    postal_code = db.Column(db.String)
+    phone = db.Column(db.String)
+    register_at = db.Column(db.DateTime)
+
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "postal_code": self.postal_code,
+            "phone": self.phone,
+            "registered_at": self.register_at
+        }
